9f1e85955318ead57977a35063997c0486b2d793
ashish <ashish.kumar2@ashishkumar2.local>
2016-03-16 14:14:46 +0530

    Initial Commit

---
+++ PayUmoneyCheckoutWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayUmoneyCheckoutWebView
+//
+//  Created by Ashish Kumar2 on 3/16/16.
+//  Copyright © 2016 Ashish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayUmoneyCheckoutWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayUmoneyCheckoutWebView
+//
+//  Created by Ashish Kumar2 on 3/16/16.
+//  Copyright © 2016 Ashish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
